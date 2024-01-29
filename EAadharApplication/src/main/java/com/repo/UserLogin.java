2f7a462137c7284a0bfbd89394ab89afb3efa5d3
Shaik Abdulrasheed <85540777+abdulrasheedshaikgit@users.noreply.github.com>
2024-01-29 09:21:19 +0530

    Delete EAadharApplication directory

--- EAadharApplication/src/main/java/com/repo/UserLogin.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entities.UserLoginEntity;
-
-public interface UserLogin extends JpaRepository<UserLoginEntity, Integer>{
-
-}
--- EAadharApplication/src/main/java/com/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entities.UserEntity;
-
-public interface UserRepo extends JpaRepository<UserEntity, Integer>{
-
-}
